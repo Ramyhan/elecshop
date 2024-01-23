34fb7ffc3e2b553b01b01a0599bda944c6c32a7f
hyunsu <hansu0809@naver.com>
2024-01-23 17:49:37 +0900

    first

---
+++ src/main/java/com/kh/elecshop/controller/HyunsuController.java
@@ -0,0 +1,20 @@
+package com.kh.elecshop.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HyunsuController {
+	
+	@GetMapping("/productList")
+	public void productList() {
+		
+	}
+	
+	@GetMapping("/product")
+	public void product() {
+		
+	}
+
+}
